471b919ee2af59f04324c10c7214b0a1983c6bbc
David Polido-Pilo <dvid_pp@yahoo.com>
2015-09-25 23:20:19 +0200

    Cleaning

--- .drush/vrt.aliases.drushrc.php
+++ .drush/vrt.aliases.drushrc.php
@@ -1,23 +1,23 @@
 <?php
 
 $aliases['radio1.dev'] = array(
-     'uri' => 'dev.radio1.be',
-     'root' => '/data/web/radio1/htdocs',
+     'uri' => 'xxx.radio1.be',
+     'root' => '/www/radio1/htdocs',
      'remote-host' => 'phpdev2.nwf',
-     'remote-user' => 'polidod',
+     'remote-user' => 'user',
      'ssh-options' => '-o PasswordAuthentication=yes',
      'path-aliases' => array(
-       '%drush-script' => '/home/polidod/drush/drush'
+       '%drush-script' => '/home/user/drush/drush'
      ),
 );
 
 $aliases['radio2.dev'] = array(
-     'uri' => 'dev.radio2.be',
-     'root' => '/data/web/radio2/htdocs',
+     'uri' => 'xxx.radio2.be',
+     'root' => '/web/radio2/htdocs',
      'remote-host' => 'phpdev2.nwf',
-     'remote-user' => 'polidod',
+     'remote-user' => 'user',
      'ssh-options' => '-o PasswordAuthentication=yes',
      'path-aliases' => array(
-       '%drush-script' => '/home/polidod/drush/drush'
+       '%drush-script' => '/home/user/drush/drush'
      ),
 );
