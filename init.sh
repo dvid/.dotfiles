c67ec64d69acb1ceba91ec4eab7fd08be7b63fea
David Polido-Pilo <dvid_pp@yahoo.com>
2016-04-14 17:00:01 +0200

    vundle as git repo + .gitignore

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #touch ~/.vimrc
-#echo "runtime .dot/.vimrc" > ~/.vimrc
+#echo "runtime! arch.vim" > ~/.vimrc
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Create symlinks calling it from within .dot/ with a symlinks folder containing all files to be symlinked in the home directory
 stow symlinks
