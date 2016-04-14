16336039c558cfb1ed0d9d5474acf2ab18852e80
David Polido-Pilo <dvid_pp@yahoo.com>
2016-04-14 17:04:27 +0200

    run pluginstall

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install Plugins
+vim +PluginInstall +qall
 
 # Create symlinks calling it from within .dot/ with a symlinks folder containing all files to be symlinked in the home directory
-stow symlinks
+#stow symlinks
